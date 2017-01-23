88cb3b826954a5868248568709252ec911f9bd11
ChaseMeImFamous <kevin.brillion@gmail.com>
2017-01-23 02:07:47 +0100

    renew docs

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Hello World
+<![CDATA[
+# ${1:MASTERMIND}
+
+## Règles du jeu
+Avant de démarrer la partie, l'ordinateur va placer des pions de couleurs (parmi celles
+proposées) derrière un cache pour éviter que le joueur ne voie les pions choisis.
+Le but est de retrouver quels sont les 4 pions choisis par ce joueur et d'en connaître les
+positions, et tout cela en 12 manches !
+Pour cela, à chaque tour, le chercheur doit se servir des pions de couleurs pour former une
+rangée et se rapprocher le plus possible de la solution (ou de la trouver) car le nombre de
+rangées pour trouver la solution est limité. Une fois les pions placés, l'ordinateur indiquera :
+1. le nombre de pions de bonne couleur bien placés en utilisant les petits pions rouges
+(ou noirs selon les versions du jeu);
+2. le nombre de pions de bonne couleur mais mal placés avec les petits pions blancs.
+## Fin d'une partie
+Deux situations terminent la partie :
+• le chercheur n'a pas réussi à trouver la solution, la partie est perdue;
+• si le chercheur a trouvé la solution, la partie est gagnée. 
+## Contributeurs
+1. Kévin Brillion
+2. Quang-Tu Dinh
+3. Paul Esdoluc
+4. Lisa Fises
+5. Antoine Turpin
+## History
+* La maquette Front est implantée
+* Le jeu est implanté
+]]>
--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,64 @@
 // Dependencies
-var gulp          = require( 'gulp' ),
-    gulp_sass     = require( 'gulp-sass' ),
-    gulp_plumber  = require('gulp-plumber'),
-    gulp_notify   = require("gulp-notify"),
-    gulp_cssnano  = require( 'gulp-cssnano' ),
-    gulp_rename   = require( 'gulp-rename' ),
-    gulp_concat   = require( 'gulp-concat' ),
-    gulp_uglify   = require( 'gulp-uglify' ),
-    browserSync   = require('browser-sync').create(),
-    autoprefixer  = require('gulp-autoprefixer'),
-    imagemin      = require('gulp-imagemin');
-
+var gulp = require('gulp')
+    , gulp_sass = require('gulp-sass')
+    , gulp_plumber = require('gulp-plumber')
+    , gulp_notify = require("gulp-notify")
+    , gulp_cssnano = require('gulp-cssnano')
+    , gulp_rename = require('gulp-rename')
+    , gulp_concat = require('gulp-concat')
+    , gulp_uglify = require('gulp-uglify')
+    , browserSync = require('browser-sync').create()
+    , autoprefixer = require('gulp-autoprefixer')
+    , imagemin = require('gulp-imagemin');
 var autoprefixerOptions = {
-  browsers : ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
+    browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
 };
-
 // Static server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
-
 // SCSS convertion task
-gulp.task( 'sass', function(){
-  return gulp.src( './assets/sass/main.scss' )   // main.scss as input
-    .pipe(gulp_plumber({errorHandler: gulp_notify.onError("Error: <%= error.message %>")}))   // Gère les erreurs
-    .pipe(gulp_sass())                  // Convert to CSS
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest( './assets/css/' ))   // Put it in CSS folder
-    .pipe(browserSync.reload({stream:true}));
-} );
-
-// min CSS task
-gulp.task( 'min_css', function(){
-  return gulp.src( './assets/css/main.css' )
-    .pipe( gulp_cssnano() )
-    .pipe( gulp_rename( 'style.min.css' ) )
-    .pipe( gulp.dest( './assets/css/' ) )
-    .pipe(browserSync.reload({stream:true}));
-} );
-
-// min JS
-gulp.task( 'js', function(){
-  return gulp.src( [
-    './assets/js/script.js',
-  ] )
-    .pipe( gulp_concat( 'script.min.js' ) )
-    .pipe( gulp_uglify() )
-    .pipe( gulp.dest( './assets/js/' ) )
-    .pipe(browserSync.reload({stream:true}));
-} );
-
-// min Images
-gulp.task('minImages', function(){
-  gulp.src('./assets/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./assets/images/'))
+gulp.task('sass', function () {
+    return gulp.src('./assets/sass/main.scss') // main.scss as input
+        .pipe(gulp_plumber({
+            errorHandler: gulp_notify.onError("Error: <%= error.message %>")
+        })) // Gère les erreurs
+        .pipe(gulp_sass()) // Convert to CSS
+        .pipe(autoprefixer(autoprefixerOptions)).pipe(gulp.dest('./assets/css/')) // Put it in CSS folder
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
-
+// min CSS task
+gulp.task('min_css', function () {
+    return gulp.src('./assets/css/main.css').pipe(gulp_cssnano()).pipe(gulp_rename('style.min.css')).pipe(gulp.dest('./assets/css/')).pipe(browserSync.reload({
+        stream: true
+    }));
+});
+// min JS
+gulp.task('js', function () {
+    return gulp.src([
+    './assets/js/script.js'
+  , ]).pipe(gulp_concat('script.min.js')).pipe(gulp_uglify()).pipe(gulp.dest('./assets/js/')).pipe(browserSync.reload({
+        stream: true
+    }));
+});
+// min Images
+gulp.task('minImages', function () {
+    gulp.src('./assets/images/*').pipe(imagemin()).pipe(gulp.dest('./assets/images/'))
+});
 // Watch task
-gulp.task( 'watch', ['browser-sync', 'sass', 'min_css', 'js', 'minImages'], function(){
-  gulp.watch( './assets/sass/main.scss', ['sass']);
-  gulp.watch( './assets/css/*.css', ['min_css']);
-  gulp.watch( [ './assets/js/**',
-               '!./assets/js/script.min.js' ],
-             ['js']);
-  gulp.watch('index.html', browserSync.reload);
-  gulp.watch('assets/css/*.css', browserSync.reload);
-  gulp.watch('assets/js/*.js', browserSync.reload);
-} );
-
+gulp.task('watch', ['browser-sync', 'sass', 'min_css', 'js', 'minImages'], function () {
+    gulp.watch('./assets/sass/main.scss', ['sass']);
+    gulp.watch('./assets/css/*.css', ['min_css']);
+    gulp.watch(['./assets/js/**'
+               , '!./assets/js/script.min.js'], ['js']);
+    gulp.watch('index.html', browserSync.reload);
+    gulp.watch('assets/css/*.css', browserSync.reload);
+    gulp.watch('assets/js/*.js', browserSync.reload);
+});
 gulp.task('default', ['sass', 'js', 'watch']);
